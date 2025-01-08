7d15cdff40d7a4ed3c1b473a431f742ffed0a0ce
PIYUSH SHAH <piyush.class@gmail.com>
2025-01-08 15:50:31 +0530

    updated

--- AddDataToDb.py
+++ AddDataToDb.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate(r"C:\Users\Genius\Desktop\Attendance System\serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred,{
     'databaseURL': "https://adr-reporting-a3c17-default-rtdb.firebaseio.com/"
