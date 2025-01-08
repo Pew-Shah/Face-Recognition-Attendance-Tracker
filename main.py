364e578bbe78263c169dbec3888b0e024ee32e5d
PIYUSH SHAH <113847277+Pew-Shah@users.noreply.github.com>
2025-01-08 21:11:43 +0530

    main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from datetime import datetime
 cred = credentials.Certificate("serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://adr-reporting-a3c17-default-rtdb.firebaseio.com/",
-    'storageBucket': "adr-reporting-a3c17.appspot.com"
+    'databaseURL': "",
+    'storageBucket': ""
 })
 
 bucket = storage.bucket()
